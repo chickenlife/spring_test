c1f18e1566c5b28f668ab1b12428526538f7dcae
chickenlife <oberlife@nate.com>
2021-03-22 23:20:07 +0900

    0322 test
    Signed-off-by: chickenlife <oberlife@nate.com>

--- spring/src/spring/di/Program.java
+++ spring/src/spring/di/Program.java
@@ -1,8 +1,13 @@
 package spring.di;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.di.entity.Exam;
+import spring.di.entity.NewlecExam;
 import spring.di.ui.ExamConsole;
 
 
@@ -10,16 +15,27 @@ public class Program {
 
 	public static void main(String[] args) {
 		/*
-		Exam exam = new NewlecExam();
+
+		//Exam exam = new NewlecExam();
+		Exam exam = new NewlecExam(10,10,10,10);
 		ExamConsole console = new GridExamConsole();
 		
 		console.setExam(exam);
 		*/
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+		Exam exam = context.getBean(Exam.class);
+		System.out.println(exam.toString());
 		ExamConsole console = (ExamConsole) context.getBean("console");
 		//ExamConsole console = context.getBean(ExamConsole.class);
 		console.print();
+		
+		List<Exam> exams = (List<Exam>) context.getBean("exams"); //new ArrayList<>();
+		//exams.add(new NewlecExam(1,2,1,1));
+		
+		for(Exam e : exams)
+			System.out.println(e);
 	} 
 
 }
